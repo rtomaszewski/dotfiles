f22ed22a84c8d66cb435d2aa4a1209d856ca83b7
Radoslaw Tomaszewski <radoslaw.tomaszewski@gmail.com>
2017-10-27 22:45:36 +0100

    Create init.lua

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,71 @@
+hs.window.animationDuration = 0
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x 
+  f.y = max.y
+  f.w = max.w 
+  f.h = max.h/2 
+  win:setFrame(f)
+end)
+
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x 
+  f.y = max.y  + (max.h/2)
+  f.w = max.w 
+  f.h = max.h/2
+  win:setFrame(f)
+end)
+
+
+
+
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x 
+  f.y = max.y 
+  f.w = max.w 
+  f.h = max.h
+  win:setFrame(f)
+end)
